16bd8e234f4e615b3e6776ef4551d4d571737225
Chris Banes <chris@banes.me>
2018-02-06 10:07:00 +0800

    Start of related shows call

--- trakt/src/main/java/me/banes/chris/tivi/trakt/calls/RelatedShowsCall.kt
+++ trakt/src/main/java/me/banes/chris/tivi/trakt/calls/RelatedShowsCall.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Google, Inc.
+ * Copyright 2018 Google, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,31 +16,48 @@
 
 package me.banes.chris.tivi.trakt.calls
 
+import com.uwetrottmann.trakt5.TraktV2
+import com.uwetrottmann.trakt5.enums.Extended
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import me.banes.chris.tivi.calls.Call
 import me.banes.chris.tivi.data.daos.TiviShowDao
 import me.banes.chris.tivi.data.entities.TiviShow
+import me.banes.chris.tivi.extensions.toRxSingle
 import me.banes.chris.tivi.trakt.TraktShowFetcher
+import me.banes.chris.tivi.trakt.state.TraktState
 import me.banes.chris.tivi.util.AppRxSchedulers
 import javax.inject.Inject
 
-class ShowDetailsCall @Inject constructor(
+class RelatedShowsCall @Inject constructor(
     private val dao: TiviShowDao,
+    private val traktState: TraktState,
     private val traktShowFetcher: TraktShowFetcher,
+    private val trakt: TraktV2,
     private val schedulers: AppRxSchedulers
-) : Call<Long, TiviShow> {
-
+) : Call<Long, List<TiviShow>> {
     override fun refresh(param: Long): Completable {
         return dao.getShowWithIdMaybe(param)
                 .subscribeOn(schedulers.database)
                 .map(TiviShow::traktId)
-                .flatMapSingle(traktShowFetcher::updateShow)
+                .flatMapSingle { traktId ->
+                    trakt.shows().related(traktId.toString(), 0, 10, Extended.NOSEASONS).toRxSingle()
+                            .subscribeOn(schedulers.network)
+                            .map {
+                                traktState.setRelatedShowsForTraktId(traktId, it.map { it.ids.trakt })
+                            }
+                }
                 .toCompletable()
     }
 
-    override fun data(param: Long): Flowable<TiviShow> {
-        return dao.getShowWithIdFlowable(param)
+    override fun data(param: Long): Flowable<List<TiviShow>> {
+        return return dao.getShowWithIdMaybe(param)
                 .subscribeOn(schedulers.database)
+                .map(TiviShow::traktId)
+                .flatMapPublisher(traktState::relatedShowsForTraktId)
+                .flatMapIterable { it }
+                .flatMapMaybe { traktShowFetcher.getShow(it) }
+                .toList()
+                .toFlowable()
     }
 }
