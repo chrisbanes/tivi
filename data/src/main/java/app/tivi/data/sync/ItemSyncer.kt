f575a62833f85d3b41d47664e4eb04f8139b2c61
Chris Banes <chrisbanes@google.com>
2018-06-04 13:08:04 +1000

    Extract watch history syncing

--- data/src/main/java/app/tivi/data/sync/ItemSyncer.kt
+++ data/src/main/java/app/tivi/data/sync/ItemSyncer.kt
@@ -18,20 +18,25 @@ package app.tivi.data.sync
 
 import app.tivi.util.Logger
 
-class Syncer<NT, ET, ID>(
-    private val currentIdsFunc: () -> Collection<ID>,
-    private val entryFetchFunc: (ID) -> ET?,
-    private val entryDeleteFunc: (ID) -> Unit,
+/**
+ * @param NT Network type
+ * @param ET local entity type
+ * @param NID Network ID type
+ */
+class ItemSyncer<NT, ET, NID>(
+    private val currentIdsFunc: () -> Collection<NID>,
+    private val entryFetchFunc: (NID) -> ET?,
+    private val entryDeleteFunc: (NID) -> Unit,
     private val entryInsertFunc: (ET) -> Long,
     private val entryUpdateFunc: (ET) -> Unit,
-    private val entityIdMapperFunc: (NT) -> ID,
+    private val entityIdMapperFunc: (NT) -> NID,
     private val entityMapperFunc: (NT) -> ET,
     private val entityMergerFunc: (ET, ET) -> ET,
     private val logger: Logger? = null
 ) {
     fun sync(networkValues: Collection<NT>) {
-        val currentIds = HashSet<ID>(currentIdsFunc())
-        logger?.d("Got current IDs as: $currentIds")
+        val currentIds = HashSet<NID>(currentIdsFunc())
+        logger?.d("Got current remote IDs as: $currentIds")
 
         networkValues.forEach { value ->
             val id = entityIdMapperFunc(value)
@@ -42,11 +47,11 @@ class Syncer<NT, ET, ID>(
                 val dbEntry = entryFetchFunc(id)!!
                 val merged = entityMergerFunc(dbEntry, newEntry)
                 entryUpdateFunc(merged)
-                logger?.d("Updated entry with id: $id")
+                logger?.d("Updated entry with remote id: $id")
             } else {
                 // Not currently in the DB, so lets insert
                 entryInsertFunc(newEntry)
-                logger?.d("Insert entry with id: $id")
+                logger?.d("Insert entry with remote id: $id")
             }
             // Finally remove the id from the set
             currentIds.remove(id)
@@ -54,7 +59,7 @@ class Syncer<NT, ET, ID>(
 
         // Anything left in the set needs to be deleted from the database
         currentIds.forEach { id ->
-            logger?.d("Remove entry with id: $id")
+            logger?.d("Remove entry with remote id: $id")
             entryDeleteFunc(id)
         }
     }
---
+++ tasks/src/main/java/app/tivi/tasks/TraktEpisodeWatchSyncer.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.tivi.tasks
+
+import app.tivi.data.DatabaseTransactionRunner
+import app.tivi.data.daos.EpisodeWatchEntryDao
+import app.tivi.data.daos.EpisodesDao
+import app.tivi.data.entities.EpisodeWatchEntry
+import app.tivi.data.sync.ItemSyncer
+import app.tivi.extensions.fetchBodyWithRetry
+import app.tivi.util.AppCoroutineDispatchers
+import app.tivi.util.Logger
+import com.uwetrottmann.trakt5.entities.HistoryEntry
+import com.uwetrottmann.trakt5.entities.UserSlug
+import com.uwetrottmann.trakt5.enums.Extended
+import com.uwetrottmann.trakt5.enums.HistoryType
+import com.uwetrottmann.trakt5.services.Users
+import kotlinx.coroutines.experimental.withContext
+import javax.inject.Inject
+import javax.inject.Provider
+
+class TraktEpisodeWatchSyncer @Inject constructor(
+    private val episodeWatchEntryDao: EpisodeWatchEntryDao,
+    private val episodesDao: EpisodesDao,
+    private val dispatchers: AppCoroutineDispatchers,
+    private val usersService: Provider<Users>,
+    private val databaseTransactionRunner: DatabaseTransactionRunner,
+    private val logger: Logger
+) {
+    private val watchSyncer = ItemSyncer(
+            episodeWatchEntryDao::entryTraktIds,
+            episodeWatchEntryDao::entryWithTraktId,
+            episodeWatchEntryDao::deleteWithTraktId,
+            episodeWatchEntryDao::insert,
+            episodeWatchEntryDao::update,
+            HistoryEntry::id,
+            ::mapToEpisodeWatchEntry,
+            { old, new -> new.copy(id = old.id) },
+            logger
+    )
+
+    suspend fun sendLocalWatchesToTrakt(traktId: Int) {
+        // TODO
+    }
+
+    suspend fun refreshWatchesFromTrakt(traktId: Int) {
+        // Fetch watched progress for show
+        val watchedProgress = withContext(dispatchers.network) {
+            // TODO use start and end dates
+            usersService.get().history(UserSlug.ME, HistoryType.SHOWS, traktId,
+                    0, 10000, Extended.NOSEASONS, null, null).fetchBodyWithRetry()
+        }
+        // and sync the result
+        syncWatchesFromTrakt(watchedProgress)
+    }
+
+    private suspend fun syncWatchesFromTrakt(watches: List<HistoryEntry>) {
+        withContext(dispatchers.database) {
+            databaseTransactionRunner.runInTransaction {
+                watchSyncer.sync(watches)
+            }
+        }
+    }
+
+    private fun mapToEpisodeWatchEntry(historyEntry: HistoryEntry): EpisodeWatchEntry {
+        val episodeTraktId = historyEntry.episode.ids.trakt
+        val episode = episodesDao.episodeWithTraktId(episodeTraktId)
+                ?: throw IllegalArgumentException("Episode with traktId[$episodeTraktId] does not exist.")
+
+        return EpisodeWatchEntry(
+                episodeId = episode.id!!,
+                traktId = historyEntry.id,
+                watchedAt = historyEntry.watched_at,
+                source = EpisodeWatchEntry.SOURCE_TRAKT
+        )
+    }
+}
