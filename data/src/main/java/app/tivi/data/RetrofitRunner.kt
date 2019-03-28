5bb66cdb9353a9605044de35e81f635a550c17b9
Chris Banes <chris@banes.me>
2019-03-28 17:24:31 +1100

    Fix RetrofitRunner now closing file streams

--- data/src/main/java/app/tivi/data/RetrofitRunner.kt
+++ data/src/main/java/app/tivi/data/RetrofitRunner.kt
@@ -32,9 +32,12 @@ class RetrofitRunner @Inject constructor() {
         return try {
             val response = request()
             if (response.isSuccessful) {
-                val okHttpNetworkResponse = response.raw().networkResponse()
-                val notModified = okHttpNetworkResponse == null || okHttpNetworkResponse.code() == 304
-                Success(data = mapper.map(response.bodyOrThrow()), responseModified = !notModified)
+                response.raw().use { rawResponse ->
+                    rawResponse.networkResponse().use { networkRawResponse ->
+                        val notModified = networkRawResponse == null || networkRawResponse.code() == 304
+                        Success(data = mapper.map(response.bodyOrThrow()), responseModified = !notModified)
+                    }
+                }
             } else {
                 ErrorResult(response.toException())
             }
