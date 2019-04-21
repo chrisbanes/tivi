cfae372bb59e479c28c043ef9e259ba042f4009c
Chris Banes <chris@banes.me>
2019-04-21 15:49:43 +1000

    Make logger varargs allow nullable values

--- app/src/main/java/app/tivi/util/TiviLogger.kt
+++ app/src/main/java/app/tivi/util/TiviLogger.kt
@@ -27,12 +27,12 @@ class TiviLogger @Inject constructor() : Logger {
         }
     }
 
-    override fun v(message: String, vararg args: Any) {
+    override fun v(message: String, vararg args: Any?) {
         Timber.v(message, args)
         logToCrashlytics(message, args)
     }
 
-    override fun v(t: Throwable, message: String, vararg args: Any) {
+    override fun v(t: Throwable, message: String, vararg args: Any?) {
         Timber.v(t, message, args)
         logToCrashlytics(message, args)
     }
@@ -41,12 +41,12 @@ class TiviLogger @Inject constructor() : Logger {
         Timber.v(t)
     }
 
-    override fun d(message: String, vararg args: Any) {
+    override fun d(message: String, vararg args: Any?) {
         Timber.d(message, args)
         logToCrashlytics(message, args)
     }
 
-    override fun d(t: Throwable, message: String, vararg args: Any) {
+    override fun d(t: Throwable, message: String, vararg args: Any?) {
         Timber.d(t, message, args)
         logToCrashlytics(message, args)
     }
@@ -55,12 +55,12 @@ class TiviLogger @Inject constructor() : Logger {
         Timber.d(t)
     }
 
-    override fun i(message: String, vararg args: Any) {
+    override fun i(message: String, vararg args: Any?) {
         Timber.i(message, args)
         logToCrashlytics(message, args)
     }
 
-    override fun i(t: Throwable, message: String, vararg args: Any) {
+    override fun i(t: Throwable, message: String, vararg args: Any?) {
         Timber.i(t, message, args)
         logToCrashlytics(message, args)
     }
@@ -69,12 +69,12 @@ class TiviLogger @Inject constructor() : Logger {
         Timber.i(t)
     }
 
-    override fun w(message: String, vararg args: Any) {
+    override fun w(message: String, vararg args: Any?) {
         Timber.w(message, args)
         logToCrashlytics(message, args)
     }
 
-    override fun w(t: Throwable, message: String, vararg args: Any) {
+    override fun w(t: Throwable, message: String, vararg args: Any?) {
         Timber.w(t, message, args)
         logToCrashlytics(message, args)
     }
@@ -83,12 +83,12 @@ class TiviLogger @Inject constructor() : Logger {
         Timber.w(t)
     }
 
-    override fun e(message: String, vararg args: Any) {
+    override fun e(message: String, vararg args: Any?) {
         Timber.e(message, args)
         logToCrashlytics(message, args)
     }
 
-    override fun e(t: Throwable, message: String, vararg args: Any) {
+    override fun e(t: Throwable, message: String, vararg args: Any?) {
         Timber.e(t, message, args)
         logToCrashlytics(message, args)
     }
@@ -97,12 +97,12 @@ class TiviLogger @Inject constructor() : Logger {
         Timber.e(t)
     }
 
-    override fun wtf(message: String, vararg args: Any) {
+    override fun wtf(message: String, vararg args: Any?) {
         Timber.wtf(message, args)
         logToCrashlytics(message, args)
     }
 
-    override fun wtf(t: Throwable, message: String, vararg args: Any) {
+    override fun wtf(t: Throwable, message: String, vararg args: Any?) {
         Timber.wtf(t, message, args)
         logToCrashlytics(message, args)
     }
@@ -111,7 +111,7 @@ class TiviLogger @Inject constructor() : Logger {
         Timber.wtf(t)
     }
 
-    private fun logToCrashlytics(message: String, vararg args: Any) {
+    private fun logToCrashlytics(message: String, vararg args: Any?) {
         if (args.isNotEmpty()) {
             Crashlytics.log(message.format(args))
         } else {
