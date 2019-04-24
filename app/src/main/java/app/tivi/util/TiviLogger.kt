692593382844bef3fd7ade5802985f5669dcaca0
Chris Banes <chris@banes.me>
2019-04-24 17:24:27 +1300

    Add custom version of Timber.DebugTree
    This version works with our wrapped calls to Timber, so
    that the log tag is correct. Timber is hard-coded to use the
    class name of the 5th stack element trace. For Tivi it's
    actually the 7th.

--- app/src/main/java/app/tivi/util/TiviLogger.kt
+++ app/src/main/java/app/tivi/util/TiviLogger.kt
@@ -16,14 +16,16 @@
 
 package app.tivi.util
 
+import android.os.Build
 import com.crashlytics.android.Crashlytics
 import timber.log.Timber
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 class TiviLogger @Inject constructor() : Logger {
     fun setup(debugMode: Boolean) {
         if (debugMode) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(TiviDebugTree())
         }
     }
 
@@ -117,5 +119,38 @@ class TiviLogger @Inject constructor() : Logger {
         } else {
             Crashlytics.log(message)
         }
+    }
+}
+
+/**
+ * Special version of [Timber.DebugTree] which is tailored for Timber being wrapped
+ * within another class.
+ */
+private class TiviDebugTree : Timber.DebugTree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        super.log(priority, createClassTag(), message, t)
+    }
+
+    private fun createClassTag(): String {
+        val stackTrace = Throwable().stackTrace
+        if (stackTrace.size <= CALL_STACK_INDEX) {
+            throw IllegalStateException("Synthetic stacktrace didn't have enough elements: are you using proguard?")
+        }
+        var tag = stackTrace[CALL_STACK_INDEX].className
+        val m = ANONYMOUS_CLASS.matcher(tag)
+        if (m.find()) {
+            tag = m.replaceAll("")
+        }
+        tag = tag.substring(tag.lastIndexOf('.') + 1)
+        // Tag length limit was removed in API 24.
+        return if (tag.length <= MAX_TAG_LENGTH || Build.VERSION.SDK_INT >= 24) {
+            tag
+        } else tag.substring(0, MAX_TAG_LENGTH)
+    }
+
+    companion object {
+        private const val MAX_TAG_LENGTH = 23
+        private const val CALL_STACK_INDEX = 7
+        private val ANONYMOUS_CLASS = Pattern.compile("(\\$\\d+)+$")
     }
 }
