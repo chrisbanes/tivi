e26c00eb72ce88abfd4d555464d6859f1cd181e5
Chris Banes <chris@banes.me>
2019-04-29 16:10:02 +1000

    Move to using CoroutineWorker

--- tasks/src/main/java/app/tivi/tasks/SyncAllFollowedShows.kt
+++ tasks/src/main/java/app/tivi/tasks/SyncAllFollowedShows.kt
@@ -17,34 +17,32 @@
 package app.tivi.tasks
 
 import android.content.Context
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import app.tivi.interactors.SyncFollowedShows
-import app.tivi.interactors.launchInteractor
 import app.tivi.tasks.inject.ChildWorkerFactory
 import app.tivi.util.Logger
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineDispatcher
 
 class SyncAllFollowedShows @AssistedInject constructor(
     @Assisted params: WorkerParameters,
     @Assisted context: Context,
     private val syncFollowedShows: SyncFollowedShows,
     private val logger: Logger
-) : Worker(context, params) {
+) : CoroutineWorker(context, params) {
     companion object {
         const val TAG = "sync-all-followed-shows"
         const val NIGHTLY_SYNC_TAG = "night-sync-all-followed-shows"
     }
 
-    override fun doWork(): Result {
-        logger.d("$TAG worker running")
+    override val coroutineContext: CoroutineDispatcher
+        get() = syncFollowedShows.dispatcher
 
-        runBlocking {
-            GlobalScope.launchInteractor(syncFollowedShows, SyncFollowedShows.ExecuteParams(true)).join()
-        }
+    override suspend fun doWork(): Result {
+        logger.d("$TAG worker running")
+        syncFollowedShows(SyncFollowedShows.ExecuteParams(true))
         return Result.success()
     }
 
