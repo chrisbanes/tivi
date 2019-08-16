f7b150561853009da3eac34ccf30a3fe172f0c56
Chris Banes <chris@banes.me>
2019-08-16 15:11:52 +1000

    Add recommended shows to data layer

---
+++ data/src/main/java/app/tivi/data/repositories/recommendedshows/TraktRecommendedShowsDataSource.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.tivi.data.repositories.recommendedshows
+
+import app.tivi.data.entities.Result
+import app.tivi.data.entities.TiviShow
+import app.tivi.data.mappers.TraktShowToTiviShow
+import app.tivi.data.mappers.toListMapper
+import app.tivi.extensions.executeWithRetry
+import app.tivi.extensions.toResult
+import com.uwetrottmann.trakt5.services.Recommendations
+import javax.inject.Inject
+import javax.inject.Provider
+
+class TraktRecommendedShowsDataSource @Inject constructor(
+    private val recommendationsService: Provider<Recommendations>,
+    private val showMapper: TraktShowToTiviShow
+) : RecommendedShowsDataSource {
+    override suspend fun getRecommendedShows(page: Int, pageSize: Int): Result<List<TiviShow>> {
+        // We add 1 because Trakt uses a 1-based index whereas we use a 0-based index
+        return recommendationsService.get().shows(null)
+                .executeWithRetry()
+                .toResult(showMapper.toListMapper())
+    }
+}
---
+++ data/src/main/java/app/tivi/data/resultentities/RecommendedEntryWithShow.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.tivi.data.resultentities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import app.tivi.data.entities.RecommendedShowEntry
+import app.tivi.data.entities.ShowTmdbImage
+import app.tivi.data.entities.TiviShow
+import java.util.Objects
+
+class RecommendedEntryWithShow : EntryWithShow<RecommendedShowEntry> {
+    @Embedded
+    override lateinit var entry: RecommendedShowEntry
+
+    @Relation(parentColumn = "show_id", entityColumn = "id")
+    override var relations: List<TiviShow> = emptyList()
+
+    @Relation(parentColumn = "show_id", entityColumn = "show_id")
+    override var images: List<ShowTmdbImage> = emptyList()
+
+    override fun equals(other: Any?): Boolean = when {
+        other === this -> true
+        other is RecommendedEntryWithShow -> {
+            entry == other.entry && relations == other.relations && images == other.images
+        }
+        else -> false
+    }
+
+    override fun hashCode(): Int = Objects.hash(entry, relations, images)
+}
