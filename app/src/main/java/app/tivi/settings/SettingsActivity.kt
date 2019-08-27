c1b4a37d716abf4ffa52d896d33fc6701a7aded8
Chris Banes <chris@banes.me>
2019-08-27 16:22:00 +1000

    Show up icon on Settings

--- app/src/main/java/app/tivi/settings/SettingsActivity.kt
+++ app/src/main/java/app/tivi/settings/SettingsActivity.kt
@@ -22,6 +22,9 @@ import androidx.appcompat.app.AppCompatActivity
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         supportFragmentManager.beginTransaction()
                 .replace(android.R.id.content, SettingsFragment())
                 .commit()
