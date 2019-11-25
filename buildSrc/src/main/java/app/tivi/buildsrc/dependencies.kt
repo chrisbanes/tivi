396d5c5557882b2151dd93587b3f0d5247b581a1
Chris Banes <chris@banes.me>
2019-11-25 13:22:37 +1100

    Update Kotlin to 1.3.60

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -66,7 +66,7 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.3.50"
+        private const val version = "1.3.60"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
