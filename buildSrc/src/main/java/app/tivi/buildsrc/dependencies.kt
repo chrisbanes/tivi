be40e7dd3f041c7c6860d9c6c33623f9a2893974
Chris Banes <chris@banes.me>
2019-12-02 09:40:12 +1100

    Use material rating bar

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -50,6 +50,8 @@ object Libs {
 
     const val flexbox = "com.google.android:flexbox:2.0.0"
 
+    const val materialRatingBar = "me.zhanghai.android.materialratingbar:library:1.3.3"
+
     object Insetter {
         private const val version = "0.2.0"
         const val dbx = "dev.chrisbanes:insetter-dbx:$version"
