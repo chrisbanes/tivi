7ae4794ffb9b639c241e8b73105d322b5306539f
Chris Banes <chris@banes.me>
2019-12-13 12:48:01 +1100

    Update AGP to 3.6.0-beta05

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:3.6.0-beta04"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.6.0-beta05"
     const val dexcountGradlePlugin = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:1.0.0"
     const val playPublisherPlugin = "com.github.triplet.gradle:play-publisher:2.6.1"
 
