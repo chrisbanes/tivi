e2182af7fef3b2862e5c3652c793c5b98412f217
Chris Banes <chris@banes.me>
2020-03-04 11:24:34 +0300

    Lots of improvements around swipe-to-dismiss
    - Now waits for the swipe animation to end before
      invoking the callback
    - Now swipe's away to the correct direction
    - Removed use of OnChildPositioned
    - `onSwipe` is now correctly called when completing swipe

--- common-ui-compose/src/main/java/app/tivi/common/compose/swipedismiss.kt
+++ common-ui-compose/src/main/java/app/tivi/common/compose/swipedismiss.kt
@@ -16,11 +16,12 @@
 
 package app.tivi.common.compose
 
+import androidx.animation.AnimationEndReason
 import androidx.compose.Composable
+import androidx.compose.onCommit
 import androidx.compose.state
 import androidx.ui.animation.animatedFloat
 import androidx.ui.core.DensityAmbient
-import androidx.ui.core.OnChildPositioned
 import androidx.ui.core.OnPositioned
 import androidx.ui.foundation.gestures.DragDirection
 import androidx.ui.foundation.gestures.Draggable
@@ -48,73 +49,65 @@ fun SwipeToDismiss(
     val position = animatedFloat(initVal = 0f).apply {
         setBounds(0f, 0f)
     }
+    var size by state { IntPxSize(IntPx.Zero, IntPx.Zero) }
+    var progress by state { 0f }
 
-    OnPositioned(onPositioned = { coordinates ->
+    OnPositioned { coordinates ->
+        size = coordinates.size
+
         if (SwipeDirection.LEFT in swipeDirections) {
             position.setBounds(-coordinates.size.width.value.toFloat(), position.max)
         }
         if (SwipeDirection.RIGHT in swipeDirections) {
             position.setBounds(position.min, coordinates.size.width.value.toFloat())
         }
-    })
-
-    val swipeChildrenSize = state { IntPxSize(IntPx.Zero, IntPx.Zero) }
-    val progress = state { 0f }
+    }
 
     if (position.value != 0f) {
-        // Container only accepts Dp values for it's size, whereas we have Px values. So we
-        // need to use WithDensity to convert them back.
-
         with(DensityAmbient.current) {
-            Container(
-                width = swipeChildrenSize.value.width.toDp(),
-                height = swipeChildrenSize.value.height.toDp(),
-                children = {
-                    backgroundChildren(
-                        progress.value,
-                        progress.value.absoluteValue >= swipeCompletePercentage
-                    )
-                }
-            )
+            Container(width = size.width.toDp(), height = size.height.toDp()) {
+                backgroundChildren(progress, progress.absoluteValue >= swipeCompletePercentage)
+            }
         }
     }
 
-    OnChildPositioned(onPositioned = { coords ->
-        swipeChildrenSize.value = coords.size
-    }) {
-        Draggable(
-            dragDirection = DragDirection.Horizontal,
-            dragValue = position,
-            onDragStopped = {
-                if (position.max > 0f && position.value / position.max >= swipeCompletePercentage) {
-                    onSwipeComplete(SwipeDirection.RIGHT)
-                } else if (position.min < 0f && position.value / position.min >= swipeCompletePercentage) {
-                    onSwipeComplete(SwipeDirection.LEFT)
+    Draggable(
+        dragDirection = DragDirection.Horizontal,
+        dragValue = position,
+        onDragStopped = {
+            when {
+                position.max > 0f && position.value / position.max >= swipeCompletePercentage -> {
+                    position.animateTo(position.max, onEnd = { endReason, _ ->
+                        if (endReason != AnimationEndReason.Interrupted) {
+                            onSwipeComplete(SwipeDirection.RIGHT)
+                        }
+                    })
                 }
-                position.animateTo(0f)
-            },
-            onDragValueChangeRequested = { dragValue ->
-                // Update the position using snapTo
-                position.snapTo(dragValue)
-
-                progress.value = when {
-                    dragValue < 0f && position.min < 0f -> dragValue / position.min
-                    dragValue > 0f && position.max > 0f -> dragValue / position.max
-                    else -> 0f
+                position.min < 0f && position.value / position.min >= swipeCompletePercentage -> {
+                    position.animateTo(position.min, onEnd = { endReason, _ ->
+                        if (endReason != AnimationEndReason.Interrupted) {
+                            onSwipeComplete(SwipeDirection.LEFT)
+                        }
+                    })
                 }
-
-                if (onSwipe != null) {
-                    // If we have an onSwipe callback, invoke it
-                    onSwipe(progress.value)
-                }
+                else -> position.animateTo(0f)
             }
-        ) {
-            WithOffset(xOffset = position) {
-                swipeChildren(
-                    progress.value,
-                    progress.value.absoluteValue >= swipeCompletePercentage
-                )
+        },
+        onDragValueChangeRequested = { position.snapTo(it) }
+    ) {
+        onCommit(position.value) {
+            // When the position changes, update the progress and call onSwipe
+            progress = when {
+                position.value < 0f && position.min < 0f -> position.value / position.min
+                position.value > 0f && position.max > 0f -> position.value / position.max
+                else -> 0f
             }
+            // If we have an onSwipe callback, invoke it
+            onSwipe?.invoke(progress)
+        }
+
+        WithOffset(xOffset = position) {
+            swipeChildren(progress, progress.absoluteValue >= swipeCompletePercentage)
         }
     }
 }
