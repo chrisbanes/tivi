dc634c5c37a3ca6831409e3a2fc7350768fba1ca
Chris Banes <chris@banes.me>
2020-03-28 11:11:47 +0000

    Bump Coroutines to 1.3.5

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -78,7 +78,7 @@ object Libs {
     }
 
     object Coroutines {
-        private const val version = "1.3.2"
+        private const val version = "1.3.5"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val rx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
