40a7da489cccdbdb277af6d08097c39828c04cfe
Chris Banes <chris@banes.me>
2020-03-28 11:09:55 +0000

    Update to Kotlin 1.3.71

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -70,7 +70,7 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.3.61"
+        private const val version = "1.3.71"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
