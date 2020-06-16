d5f0367486e388a179b8338836f7cecf2195f9dc
Chris Banes <chris@banes.me>
2020-06-16 14:17:34 +0100

    Revert back to Epoxy 3.11.0

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -196,7 +196,7 @@ object Libs {
     }
 
     object Epoxy {
-        private const val version = "4.0.0-beta4"
+        private const val version = "3.11.0"
         const val epoxy = "com.airbnb.android:epoxy:$version"
         const val paging = "com.airbnb.android:epoxy-paging:$version"
         const val dataBinding = "com.airbnb.android:epoxy-databinding:$version"
