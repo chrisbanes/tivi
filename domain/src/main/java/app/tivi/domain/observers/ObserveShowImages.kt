327ce93bdb5876abb4ceb67c0a9527bd0e805498
Chris Banes <chris@banes.me>
2020-06-23 13:32:31 +0100

    Force Store stream() to flow on background thread

--- domain/src/main/java/app/tivi/domain/observers/ObserveShowImages.kt
+++ domain/src/main/java/app/tivi/domain/observers/ObserveShowImages.kt
@@ -19,18 +19,22 @@ package app.tivi.domain.observers
 import app.tivi.data.entities.ShowImages
 import app.tivi.data.repositories.showimages.ShowImagesStore
 import app.tivi.domain.SubjectInteractor
+import app.tivi.util.AppCoroutineDispatchers
 import com.dropbox.android.external.store4.StoreRequest
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ObserveShowImages @Inject constructor(
-    private val store: ShowImagesStore
+    private val store: ShowImagesStore,
+    private val dispatchers: AppCoroutineDispatchers
 ) : SubjectInteractor<ObserveShowImages.Params, ShowImages>() {
 
     override fun createObservable(params: Params): Flow<ShowImages> {
         return store.stream(StoreRequest.cached(params.showId, refresh = false))
             .map { ShowImages(it.requireData()) }
+            .flowOn(dispatchers.computation)
     }
 
     data class Params(val showId: Long)
