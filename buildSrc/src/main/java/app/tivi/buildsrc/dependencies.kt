2fcd9153012240771f8f80eaa20ba74c1a735e63
Chris Banes <chrisbanes@users.noreply.github.com>
2020-10-19 15:26:28 +0100

    Use Gradle Play Publisher SNAPSHOT (#714)
    * Try out Gradle Play Publisher SNAPSHOT
    
    Includes workaround for
    https://github.com/Triple-T/gradle-play-publisher/issues/864

--- buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/app/tivi/buildsrc/dependencies.kt
@@ -21,9 +21,9 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha11"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha13"
 
-    const val gradlePlayPublisher = "com.github.triplet.gradle:play-publisher:3.0.0"
+    const val gradlePlayPublisher = "com.github.triplet.gradle:play-publisher:3.2.0-SNAPSHOT"
 
     const val threeTenBp = "org.threeten:threetenbp:1.4.4"
     const val threeTenBpNoTzdb = "$threeTenBp:no-tzdb"
