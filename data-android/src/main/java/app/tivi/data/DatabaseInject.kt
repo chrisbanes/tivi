a74b40f5c9bab5fe0f4f76e5965811bbf6da0bb7
Chris Banes <chrisbanes@users.noreply.github.com>
2021-05-06 10:35:54 +0100

    Try out Room auto-migrations (#797)
    See https://medium.com/androiddevelopers/room-auto-migrations-d5370b0ca6eb for more information.

--- data-android/src/main/java/app/tivi/data/DatabaseInject.kt
+++ data-android/src/main/java/app/tivi/data/DatabaseInject.kt
@@ -36,7 +36,6 @@ object RoomDatabaseModule {
         @ApplicationContext context: Context
     ): TiviRoomDatabase {
         val builder = Room.databaseBuilder(context, TiviRoomDatabase::class.java, "shows.db")
-            .addMigrations(*TiviRoomDatabase_Migrations.build())
             .fallbackToDestructiveMigration()
         if (Debug.isDebuggerConnected()) {
             builder.allowMainThreadQueries()
