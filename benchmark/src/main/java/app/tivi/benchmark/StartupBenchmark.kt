e659f3bfce701092454ba945bf549f5730e4659b
Chris Banes <chris@banes.me>
2022-07-12 12:19:15 +0100

    Add working baseline profile

--- benchmark/src/main/java/app/tivi/benchmark/StartupBenchmark.kt
+++ benchmark/src/main/java/app/tivi/benchmark/StartupBenchmark.kt
@@ -24,20 +24,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * This is an example startup benchmark.
- *
- * It navigates to the device's home screen, and launches the default activity.
- *
- * Before running this benchmark:
- * 1) switch your app's active build variant in the Studio (affects Studio runs only)
- * 2) add `<profileable android:shell="true" />` to your app's manifest, within the `<application>` tag
- *
- * Run this benchmark from Studio to see startup measurements, and captured system traces
- * for investigating your app's performance.
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleStartupBenchmark {
+class StartupBenchmark {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
