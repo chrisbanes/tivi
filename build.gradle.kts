7bd6ceb10d17adef99979f15432219169b2062da
Goooler <wangzongler@gmail.com>
2022-09-26 09:54:46 +0800

    Replace room's kapt with ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ plugins {
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.gms.googleServices) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
