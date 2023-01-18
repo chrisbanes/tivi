9400984df3a380e3a56957fcf2560f2f7972b2dd
Chris Banes <chris@banes.me>
2023-01-18 12:15:56 +0000

    Use ReportDrawnWhen on ui-discover

--- ui/discover/build.gradle.kts
+++ ui/discover/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
 
     implementation(libs.compose.foundation.foundation)
     implementation(libs.compose.foundation.layout)
