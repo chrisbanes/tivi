dff6175efa4361e6042fa62c6b093c25f06e3bac
Chris Banes <chris@banes.me>
2023-03-30 08:59:50 +0700

    Explicitly set root project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,6 +39,8 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 // https://docs.gradle.org/7.6/userguide/configuration_cache.html#config_cache:stable
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
+rootProject.name = "tivi"
+
 include(
     ":core:analytics",
     ":core:base",
