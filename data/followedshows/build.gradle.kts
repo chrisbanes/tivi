01927f31c820b89268bb84d4aa2f516bbdecf169
Chris Banes <chris@banes.me>
2023-03-31 16:28:25 +0700

    Migrate :data:followedshows to KMP

--- data/followedshows/build.gradle.kts
+++ data/followedshows/build.gradle.kts
@@ -16,23 +16,34 @@
 
 
 plugins {
-    id("kotlin")
-    alias(libs.plugins.android.lint)
+    kotlin("multiplatform")
+    alias(libs.plugins.cacheFixPlugin)
     alias(libs.plugins.ksp)
 }
 
+kotlin {
+    jvm()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(projects.data.models)
+                api(projects.data.traktauth)
+                implementation(projects.data.db)
+                implementation(projects.data.legacy) // remove this eventually
+
+                api(projects.api.trakt)
+                api(projects.api.tmdb)
+
+                api(libs.store)
+                implementation(libs.kotlinx.atomicfu)
+
+                implementation(libs.kotlininject.runtime)
+            }
+        }
+    }
+}
+
 dependencies {
-    api(projects.data.models)
-    api(projects.data.traktauth)
-    implementation(projects.data.db)
-    implementation(projects.data.legacy) // remove this eventually
-
-    api(projects.api.trakt)
-    api(projects.api.tmdb)
-
-    api(libs.store)
-    implementation(libs.kotlinx.atomicfu)
-
-    implementation(libs.kotlininject.runtime)
-    ksp(libs.kotlininject.compiler)
+    add("kspJvm", libs.kotlininject.compiler)
 }
