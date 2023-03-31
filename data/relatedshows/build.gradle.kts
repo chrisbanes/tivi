77d35d217b1977d3248c56a09138934b1d644bdb
Chris Banes <chris@banes.me>
2023-03-31 16:50:39 +0700

    Migrate :data:relatedshows to KMP

--- data/relatedshows/build.gradle.kts
+++ data/relatedshows/build.gradle.kts
@@ -14,40 +14,34 @@
  * limitations under the License.
  */
 
-
-/*
- * Copyright 2017 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 plugins {
-    id("kotlin")
-    alias(libs.plugins.android.lint)
+    kotlin("multiplatform")
+    alias(libs.plugins.cacheFixPlugin)
     alias(libs.plugins.ksp)
 }
 
+kotlin {
+    jvm()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(projects.data.models)
+                implementation(projects.data.db)
+                implementation(projects.data.legacy) // remove this eventually
+
+                implementation(projects.api.trakt)
+                implementation(projects.api.tmdb)
+
+                api(libs.store)
+                implementation(libs.kotlinx.atomicfu)
+
+                implementation(libs.kotlininject.runtime)
+            }
+        }
+    }
+}
+
 dependencies {
-    api(projects.data.models)
-    implementation(projects.data.db)
-    implementation(projects.data.legacy) // remove this eventually
-
-    implementation(projects.api.trakt)
-    implementation(projects.api.tmdb)
-
-    api(libs.store)
-    implementation(libs.kotlinx.atomicfu)
-
-    implementation(libs.kotlininject.runtime)
-    ksp(libs.kotlininject.compiler)
+    add("kspJvm", libs.kotlininject.compiler)
 }
