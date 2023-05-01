e77f3cca98718d5cef4637f23826af373b17d6f0
Chris Banes <chris@banes.me>
2023-05-01 14:42:46 +0100

    Implement seasonsWithEpisodesForShowId

--- data/db-sqldelight/src/commonMain/kotlin/app/tivi/data/daos/SqlDelightSeasonsDao.kt
+++ data/db-sqldelight/src/commonMain/kotlin/app/tivi/data/daos/SqlDelightSeasonsDao.kt
@@ -17,18 +17,27 @@
 package app.tivi.data.daos
 
 import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import app.cash.sqldelight.coroutines.mapToOne
 import app.tivi.data.Database
 import app.tivi.data.awaitAsNull
 import app.tivi.data.awaitList
+import app.tivi.data.compoundmodels.EpisodeWithWatches
 import app.tivi.data.compoundmodels.SeasonWithEpisodesAndWatches
+import app.tivi.data.models.Episode
+import app.tivi.data.models.EpisodeWatchEntry
 import app.tivi.data.models.Season
 import app.tivi.data.upsert
 import app.tivi.util.AppCoroutineDispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import me.tatarka.inject.annotations.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Inject
 class SqlDelightSeasonsDao(
     override val db: Database,
@@ -78,10 +87,46 @@ class SqlDelightSeasonsDao(
     )
 
     override fun seasonsWithEpisodesForShowId(showId: Long): Flow<List<SeasonWithEpisodesAndWatches>> {
-        TODO()
-//        return db.seasonsQueries.seasonsForShowId(showId)
-//            .asFlow()
-//            .mapToList(dispatchers.io)
+        return db.seasonsQueries.seasonsForShowId(showId, ::Season)
+            .asFlow()
+            .mapToList(dispatchers.io)
+            // This should be flatMapLatest
+            .flatMapLatest { seasons ->
+                val flows = seasons.map { season ->
+                    db.episodesQueries.episodesWithSeasonId(season.id, ::Episode)
+                        .asFlow()
+                        .mapToList(dispatchers.io)
+                        .map { episodes -> season to episodes }
+                        .flatMapLatest { (season, episodes) ->
+                            val watchFlows = episodes.map { episode ->
+                                db.episode_watch_entriesQueries.watchesForEpisodeId(
+                                    episodeId = episode.id,
+                                    mapper = ::EpisodeWatchEntry,
+                                )
+                                    .asFlow()
+                                    .mapToList(dispatchers.io)
+                                    .map { watches -> episode to watches }
+                            }
+
+                            combine(watchFlows) { combined ->
+                                combined.map { (episode, watches) ->
+                                    EpisodeWithWatches().apply {
+                                        this.episode = episode
+                                        this.watches = watches
+                                    }
+                                }
+                            }.map { episodesWithWatches ->
+                                SeasonWithEpisodesAndWatches().apply {
+                                    this.season = season
+                                    this.episodes = episodesWithWatches
+                                }
+                            }
+                        }
+                }
+                combine(flows) { combined ->
+                    combined.map { it }
+                }
+            }
     }
 
     override fun observeSeasonWithId(id: Long): Flow<Season> {
