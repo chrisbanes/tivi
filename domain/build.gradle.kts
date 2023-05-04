4274ced7fe0011f7bb7fc723b15416fde0666780
Chris Banes <chris@banes.me>
2023-05-04 20:16:39 +0100

    Migrate :domain to KMP plugin

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -16,9 +16,9 @@
 
 
 plugins {
-    id("kotlin")
-    alias(libs.plugins.cacheFixPlugin)
+    kotlin("multiplatform")
     alias(libs.plugins.ksp)
+    alias(libs.plugins.cacheFixPlugin)
 }
 
 // https://github.com/cashapp/multiplatform-paging/issues/6
@@ -26,32 +26,43 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask<*>> {
     compilerOptions.freeCompilerArgs.add("-opt-in=androidx.paging.ExperimentalPagingApi")
 }
 
+kotlin {
+    jvm()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(projects.core.base)
+
+                api(projects.data.models)
+                implementation(projects.data.db) // remove this eventually
+                api(projects.data.legacy) // remove this eventually
+
+                api(projects.tasks.api)
+
+                api(projects.data.episodes)
+                api(projects.data.followedshows)
+                api(projects.data.popularshows)
+                api(projects.data.recommendedshows)
+                api(projects.data.relatedshows)
+                api(projects.data.search)
+                api(projects.data.showimages)
+                api(projects.data.shows)
+                api(projects.data.traktauth)
+                api(projects.data.traktusers)
+                api(projects.data.trendingshows)
+                api(projects.data.watchedshows)
+
+                implementation(projects.api.tmdb)
+
+                api(libs.cashapp.paging.common)
+
+                implementation(libs.kotlininject.runtime)
+            }
+        }
+    }
+}
+
 dependencies {
-    implementation(projects.core.base)
-
-    api(projects.data.models)
-    implementation(projects.data.db) // remove this eventually
-    api(projects.data.legacy) // remove this eventually
-
-    api(projects.tasks.api)
-
-    api(projects.data.episodes)
-    api(projects.data.followedshows)
-    api(projects.data.popularshows)
-    api(projects.data.recommendedshows)
-    api(projects.data.relatedshows)
-    api(projects.data.search)
-    api(projects.data.showimages)
-    api(projects.data.shows)
-    api(projects.data.traktauth)
-    api(projects.data.traktusers)
-    api(projects.data.trendingshows)
-    api(projects.data.watchedshows)
-
-    implementation(projects.api.tmdb)
-
-    api(libs.cashapp.paging.common)
-
-    implementation(libs.kotlininject.runtime)
-    ksp(libs.kotlininject.compiler)
+    add("kspJvm", libs.kotlininject.compiler)
 }
