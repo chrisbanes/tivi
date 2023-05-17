4f813454861eb120a13bc92f80583e11129a9cfd
Chris Banes <chris@banes.me>
2023-05-17 10:41:43 +0100

    Enable iOS targets for all KMP libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,8 +13,6 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
     repositories {
         google()
         mavenCentral()
