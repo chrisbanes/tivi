8c34bb2c87e69215b41f2628409b8743465e581b
Chris Banes <chris@banes.me>
2023-05-29 11:39:23 +0100

    Tweak Android Studio copyright settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,4 @@
-// Copyright 2023, Google LLC, Christopher Banes and the Tivi project contributors
-// SPDX-License-Identifier: Apache-2.0
-
-// Copyright 2017, Google LLC, Christopher Banes and the Tivi project contributors
+// Copyright 2023, Christopher Banes and the Tivi project contributors
 // SPDX-License-Identifier: Apache-2.0
 
 pluginManagement {
