1a5179c6564579fa3e3e5697dc090b1f10daee15
Chris Banes <chris@banes.me>
2023-05-31 13:37:19 +0100

    Simplify KotlinMultiplatformConventionPlugin

--- gradle/build-logic/convention/src/main/java/app/tivi/gradle/KotlinMultiplatformConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/java/app/tivi/gradle/KotlinMultiplatformConventionPlugin.kt
@@ -3,17 +3,13 @@
 
 package app.tivi.gradle
 
-import org.gradle.api.Action
-import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.creating
-import org.gradle.kotlin.dsl.getValue
-import org.gradle.kotlin.dsl.getting
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
@@ -22,41 +18,15 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
         }
 
         extensions.configure<KotlinMultiplatformExtension> {
-            jvm()
+            targetHierarchy.default()
 
             listOf(
                 iosX64(),
                 iosArm64(),
                 iosSimulatorArm64(),
-            ).forEach {
-                it.binaries.framework {
+            ).forEach { target ->
+                target.binaries.framework {
                     baseName = path.replace(':', '-')
-                }
-            }
-
-            sourceSets {
-                val commonMain by getting
-                val commonTest by getting
-
-                val iosX64Main by getting
-                val iosArm64Main by getting
-                val iosSimulatorArm64Main by getting
-
-                val iosMain by creating {
-                    dependsOn(commonMain)
-                    iosX64Main.dependsOn(this)
-                    iosArm64Main.dependsOn(this)
-                    iosSimulatorArm64Main.dependsOn(this)
-                }
-                val iosX64Test by getting
-                val iosArm64Test by getting
-                val iosSimulatorArm64Test by getting
-
-                val iosTest by creating {
-                    dependsOn(commonTest)
-                    iosX64Test.dependsOn(this)
-                    iosArm64Test.dependsOn(this)
-                    iosSimulatorArm64Test.dependsOn(this)
                 }
             }
 
@@ -64,6 +34,3 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
         }
     }
 }
-
-fun KotlinMultiplatformExtension.sourceSets(configure: Action<NamedDomainObjectContainer<KotlinSourceSet>>): Unit =
-    (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("sourceSets", configure)
