3c573430a831263f405c0f7159d18c93e4e0af26
Chris Banes <chris@banes.me>
2023-06-07 13:46:13 +0100

    Move common AnalyticsComponent

---
+++ core/analytics/src/iosMain/kotlin/app/tivi/core/analytics/AnalyticsComponent.kt
@@ -0,0 +1,15 @@
+// Copyright 2023, Christopher Banes and the Tivi project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package app.tivi.core.analytics
+
+import app.tivi.inject.ApplicationScope
+import me.tatarka.inject.annotations.Provides
+
+actual interface AnalyticsComponent {
+    @Provides
+    @ApplicationScope
+    fun provideAnalytics(): Analytics = object : Analytics {
+        override fun trackScreenView(name: String, arguments: Map<String, *>?) = Unit
+    }
+}
