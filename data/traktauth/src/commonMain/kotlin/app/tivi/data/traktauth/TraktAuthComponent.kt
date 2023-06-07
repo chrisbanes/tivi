dd35397b4d2e9c37abf5dc3b5e4e5f8ee079f8a0
Chris Banes <chris@banes.me>
2023-06-07 13:48:59 +0100

    Move common TraktAuthComponent

---
+++ data/traktauth/src/commonMain/kotlin/app/tivi/data/traktauth/TraktAuthComponent.kt
@@ -0,0 +1,6 @@
+// Copyright 2017, Google LLC, Christopher Banes and the Tivi project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package app.tivi.data.traktauth
+
+expect interface TraktAuthComponent
