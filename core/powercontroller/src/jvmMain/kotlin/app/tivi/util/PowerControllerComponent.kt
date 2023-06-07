679d8a7877100234887e204c71d8d11639fa4fb6
Chris Banes <chris@banes.me>
2023-06-07 13:21:08 +0100

    Move common PowerControllerComponent

--- core/powercontroller/src/jvmMain/kotlin/app/tivi/util/PowerControllerComponent.kt
+++ core/powercontroller/src/jvmMain/kotlin/app/tivi/util/PowerControllerComponent.kt
@@ -1,4 +1,4 @@
-// Copyright 2023, Google LLC, Christopher Banes and the Tivi project contributors
+// Copyright 2023, Christopher Banes and the Tivi project contributors
 // SPDX-License-Identifier: Apache-2.0
 
 package app.tivi.util
@@ -7,5 +7,5 @@ import me.tatarka.inject.annotations.Provides
 
 actual interface PowerControllerComponent {
     @Provides
-    fun providePowerController(bind: AndroidPowerController): PowerController = bind
+    fun providePowerController() = EmptyPowerController
 }
