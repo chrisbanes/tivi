8e1c181b63c2c7354a8ef9a87aa20a494f6544d8
Chris Banes <chris@banes.me>
2023-06-28 11:29:44 +0100

    Move use of Android's dynamic color scheme to androidMain

---
+++ common/ui/compose/src/androidMain/kotlin/app/tivi/common/compose/theme/Platform.kt
@@ -0,0 +1,26 @@
+// Copyright 2023, Christopher Banes and the Tivi project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package app.tivi.common.compose.theme
+
+import android.os.Build
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+internal actual fun colorScheme(
+    useDarkColors: Boolean,
+    useDynamicColors: Boolean,
+): ColorScheme = when {
+    Build.VERSION.SDK_INT >= 31 && useDynamicColors && useDarkColors -> {
+        dynamicDarkColorScheme(LocalContext.current)
+    }
+    Build.VERSION.SDK_INT >= 31 && useDynamicColors && !useDarkColors -> {
+        dynamicLightColorScheme(LocalContext.current)
+    }
+    useDarkColors -> TiviDarkColors
+    else -> TiviLightColors
+}
--- common/ui/compose/src/commonMain/kotlin/app/tivi/common/compose/theme/Theme.kt
+++ common/ui/compose/src/commonMain/kotlin/app/tivi/common/compose/theme/Theme.kt
@@ -3,13 +3,9 @@
 
 package app.tivi.common.compose.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun TiviTheme(
@@ -18,16 +14,7 @@ fun TiviTheme(
     content: @Composable () -> Unit,
 ) {
     MaterialTheme(
-        colorScheme = when {
-            Build.VERSION.SDK_INT >= 31 && useDynamicColors && useDarkColors -> {
-                dynamicDarkColorScheme(LocalContext.current)
-            }
-            Build.VERSION.SDK_INT >= 31 && useDynamicColors && !useDarkColors -> {
-                dynamicLightColorScheme(LocalContext.current)
-            }
-            useDarkColors -> TiviDarkColors
-            else -> TiviLightColors
-        },
+        colorScheme = colorScheme(useDarkColors, useDynamicColors),
         typography = TiviTypography,
         shapes = TiviShapes,
         content = content,
