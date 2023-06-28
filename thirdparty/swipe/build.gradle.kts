be3220a87c4c5cb273ccaefc85f083044e234f5b
Chris Banes <chris@banes.me>
2023-06-28 20:38:48 +0100

    Import github.com/saket/swipe
    We need to build it again Compose MP

---
+++ thirdparty/swipe/build.gradle.kts
@@ -0,0 +1,23 @@
+// Copyright 2023, Christopher Banes and the Tivi project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+
+plugins {
+    id("app.tivi.android.library")
+    id("app.tivi.kotlin.multiplatform")
+    alias(libs.plugins.composeMultiplatform)
+}
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.foundation)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "me.saket.swipe"
+}
