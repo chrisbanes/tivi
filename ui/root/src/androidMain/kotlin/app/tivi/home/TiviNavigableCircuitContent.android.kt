94fd3bf75997a5048c6403e52eadd1b6683f39f1
Chris Banes <chris@banes.me>
2023-07-10 14:48:48 +0100

    Android Predictive back treatment now implements motion specs
    We don't implement the vertical shifting (yet), but that can wait.
    https://developer.android.com/design/ui/mobile/guides/patterns/predictive-back

--- ui/root/src/androidMain/kotlin/app/tivi/home/TiviNavigableCircuitContent.android.kt
+++ ui/root/src/androidMain/kotlin/app/tivi/home/TiviNavigableCircuitContent.android.kt
@@ -8,26 +8,27 @@ import android.os.Build
 import android.window.BackEvent
 import android.window.OnBackAnimationCallback
 import android.window.OnBackInvokedDispatcher
-import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import app.tivi.animations.lerp
 import com.slack.circuit.backstack.BackStack
 import com.slack.circuit.backstack.ProvidedValues
 import com.slack.circuit.backstack.SaveableBackStack
@@ -62,42 +63,122 @@ actual fun TiviNavigableCircuitContent(
     }
 
     var progress by remember { mutableStateOf(0f) }
-
-    val onBackInvokedDispatcher = LocalView.current.findOnBackInvokedDispatcher()
     var backStackAnimationEnabled by remember { mutableStateOf(false) }
 
     LaunchedEffect(backstack) {
         snapshotFlow { backstack.isAtRoot }
             .collect { atRoot ->
                 backStackAnimationEnabled = !atRoot
+                if (atRoot) {
+                    progress = 0f
+                }
             }
     }
+
+    BackHandler(
+        animatedEnabled = backStackAnimationEnabled,
+        onBackProgress = { progress = it },
+        onBackInvoked = {
+            navigator.pop()
+            progress = 0f
+        },
+    )
+
+    val activeContentProviders = backstack.buildCircuitContentProviders(
+        navigator = navigator,
+        circuitConfig = circuitConfig,
+        unavailableRoute = unavailableRoute,
+    )
+
+    Box(modifier = modifier) {
+        val current = activeContentProviders.first()
+        val previous = activeContentProviders.getOrNull(1)
+
+        previous?.let { cp ->
+            // movableContentOf needs to be attached to the composition otherwise it is cleaned up.
+            // Ideally we wouldn't call PreviousContent when `showPrevious` is false, but then we
+            // lose all of the presenter state (as the movable content is cleaned-up).
+            Box(modifier = Modifier.zIndex(0f)) {
+                val values = providedValues[cp.backStackRecord]?.provideValues()
+                val providedLocals = remember(values) {
+                    values?.toTypedArray() ?: emptyArray()
+                }
+                CompositionLocalProvider(*providedLocals) {
+                    cp.content(cp.backStackRecord)
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .zIndex(1f)
+                .predictiveBackMotionTreatment(MaterialTheme.shapes.extraLarge) { progress },
+        ) {
+            val values = providedValues[current.backStackRecord]?.provideValues()
+            val providedLocals = remember(values) {
+                values?.toTypedArray() ?: emptyArray()
+            }
+            CompositionLocalProvider(*providedLocals) {
+                current.content(current.backStackRecord)
+            }
+        }
+    }
+}
+
+private fun Modifier.predictiveBackMotionTreatment(
+    shape: Shape,
+    progressProvider: () -> Float,
+): Modifier = graphicsLayer {
+    val progress = progressProvider()
+    // If we're at progress 0f, skip setting any parameters
+    if (progress == 0f) return@graphicsLayer
+
+    translationX = -(8.dp * progress).toPx()
+    shadowElevation = 6.dp.toPx()
+
+    val scale = lerp(1f, 0.9f, progress.absoluteValue)
+    scaleX = scale
+    scaleY = scale
+    transformOrigin = TransformOrigin(
+        pivotFractionX = if (progress > 0) 1f else 0f,
+        pivotFractionY = 0.5f,
+    )
+
+    // TODO: interpolate from rectangle to shape?
+    this.shape = shape
+    clip = true
+}
+
+@RequiresApi(34)
+@Composable
+private fun BackHandler(
+    onBackProgress: (Float) -> Unit,
+    animatedEnabled: Boolean = true,
+    onBackInvoked: () -> Unit,
+) {
+    val onBackInvokedDispatcher = LocalView.current.findOnBackInvokedDispatcher()
+    val lastAnimatedEnabled by rememberUpdatedState(animatedEnabled)
 
     DisposableEffect(onBackInvokedDispatcher) {
         val callback = object : OnBackAnimationCallback {
             override fun onBackStarted(backEvent: BackEvent) {
-                progress = 0f
-            }
-
-            override fun onBackProgressed(backEvent: BackEvent) {
-                if (!backStackAnimationEnabled) return
-
-                when (backEvent.swipeEdge) {
-                    BackEvent.EDGE_LEFT -> {
-                        progress = backEvent.progress
-                    }
-
-                    else -> {
-                        progress = -backEvent.progress
-                    }
+                if (lastAnimatedEnabled) {
+                    onBackProgress(0f)
                 }
             }
 
-            override fun onBackInvoked() {
-                // TODO finish off animation before pop()
-                navigator.pop()
-                progress = 0f
+            override fun onBackProgressed(backEvent: BackEvent) {
+                if (lastAnimatedEnabled) {
+                    onBackProgress(
+                        when (backEvent.swipeEdge) {
+                            BackEvent.EDGE_LEFT -> backEvent.progress
+                            else -> -backEvent.progress
+                        },
+                    )
+                }
             }
+
+            override fun onBackInvoked() = onBackInvoked()
         }
 
         onBackInvokedDispatcher?.registerOnBackInvokedCallback(
@@ -109,69 +190,6 @@ actual fun TiviNavigableCircuitContent(
 
         onDispose {
             onBackInvokedDispatcher?.unregisterOnBackInvokedCallback(callback)
-        }
-    }
-
-    Box(modifier = modifier) {
-        val activeContentProviders = backstack.buildCircuitContentProviders(
-            navigator = navigator,
-            circuitConfig = circuitConfig,
-            unavailableRoute = unavailableRoute,
-        )
-
-        val topContentProvider = activeContentProviders.first().let {
-            remember(it.backStackRecord.key) { it }
-        }
-        val previousContentProvider = activeContentProviders.getOrNull(1)?.let {
-            remember(it.backStackRecord.key) { it }
-        }
-
-        val showPrevious by remember {
-            derivedStateOf { progress != 0f }
-        }
-
-        if (showPrevious) {
-            previousContentProvider?.let { cp ->
-                Box(modifier = Modifier.zIndex(0f)) {
-                    val values = providedValues[cp.backStackRecord]?.provideValues()
-                    val providedLocals = remember(values) {
-                        values?.toTypedArray() ?: emptyArray()
-                    }
-                    CompositionLocalProvider(*providedLocals) {
-                        cp.content(cp.backStackRecord)
-                    }
-                }
-            }
-        }
-
-        val shape = MaterialTheme.shapes.extraLarge
-        Box(
-            modifier = Modifier
-                .zIndex(if (showPrevious) 1f else 0f)
-                .graphicsLayer {
-                    val eased = FastOutSlowInEasing.transform(progress)
-
-                    translationX = -(48.dp * eased).toPx()
-                    scaleX = 1f - eased.absoluteValue
-                    scaleY = scaleX
-                    shadowElevation = if (eased != 0f) 6.dp.toPx() else 0f
-                    transformOrigin = TransformOrigin(
-                        pivotFractionX = if (eased > 0) 1f else 0f,
-                        pivotFractionY = 0.5f,
-                    )
-                    clip = eased != 0f
-
-                    // TODO: interpolate from rectangle to shape?
-                    this.shape = if (eased != 0f) shape else RectangleShape
-                },
-        ) {
-            val values = providedValues[topContentProvider.backStackRecord]?.provideValues()
-            val providedLocals = remember(values) {
-                values?.toTypedArray() ?: emptyArray()
-            }
-            CompositionLocalProvider(*providedLocals) {
-                topContentProvider.content(topContentProvider.backStackRecord)
-            }
         }
     }
 }
