828290fcd0290fac66c29993a1d070a1acaa220d
Chris Banes <chris@banes.me>
2023-07-14 14:33:29 +0100

    Re-enable Gradle configuration cache :partyparrot:

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -66,7 +66,7 @@ buildCache {
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 // https://docs.gradle.org/7.6/userguide/configuration_cache.html#config_cache:stable
-// enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
 rootProject.name = "tivi"
 
