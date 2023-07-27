83c696e98b3466372ffe43672e720e5f9f5948d2
Chris Banes <chris@banes.me>
2023-07-27 21:38:01 +0100

    Remove our custom touch slop
    This was added in #1422 as a workaround, but it has been
    fixed upstream.

--- ui/root/src/iosMain/kotlin/app/tivi/home/TiviUiViewController.kt
+++ ui/root/src/iosMain/kotlin/app/tivi/home/TiviUiViewController.kt
@@ -3,13 +3,8 @@
 
 package app.tivi.home
 
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.LocalUIViewController
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalViewConfiguration
-import androidx.compose.ui.platform.ViewConfiguration
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import app.tivi.screens.DiscoverScreen
 import com.slack.circuit.backstack.rememberSaveableBackStack
@@ -28,31 +23,15 @@ fun TiviUiViewController(
 ): UIViewController = ComposeUIViewController {
     val backstack = rememberSaveableBackStack { push(DiscoverScreen) }
     val navigator = rememberCircuitNavigator(backstack, onRootPop = { /* no-op */ })
-
     val uiViewController = LocalUIViewController.current
 
-    // Increase the touch slop. The default value of 3.dp is a bit low imo, so we use the
-    // Android default value of 8.dp
-    // https://github.com/JetBrains/compose-multiplatform/issues/3397
-    val vc = LocalViewConfiguration.current.withTouchSlop(
-        with(LocalDensity.current) { 8.dp.toPx() },
+    tiviContent(
+        backstack,
+        navigator,
+        { url ->
+            val safari = SFSafariViewController(NSURL(string = url))
+            uiViewController.presentViewController(safari, animated = true, completion = null)
+        },
+        Modifier,
     )
-
-    CompositionLocalProvider(LocalViewConfiguration provides vc) {
-        tiviContent(
-            backstack,
-            navigator,
-            { url ->
-                val safari = SFSafariViewController(NSURL(string = url))
-                uiViewController.presentViewController(safari, animated = true, completion = null)
-            },
-            Modifier,
-        )
-    }
-}
-
-private fun ViewConfiguration.withTouchSlop(
-    touchSlop: Float,
-): ViewConfiguration = object : ViewConfiguration by this {
-    override val touchSlop: Float = touchSlop
 }
