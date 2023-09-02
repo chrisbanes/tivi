5bf82ccb154015a2e9d6cad74525379cb78c5aa0
Chris Banes <chrisbanes@users.noreply.github.com>
2023-09-02 08:05:06 +0100

    Bump Android app version

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         applicationId = "app.tivi"
         versionCode = appVersionCode
-        versionName = "0.9.6"
+        versionName = "0.9.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
