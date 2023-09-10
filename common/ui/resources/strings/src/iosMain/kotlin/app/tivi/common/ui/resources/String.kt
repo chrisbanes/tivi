5fad98e3e8228d56d09a2b7f1592051cfac9dc53
Chris Banes <chrisbanes@users.noreply.github.com>
2023-09-10 13:43:01 +0300

    Hopefully fix string formatting on iOS (#1525)

--- common/ui/resources/strings/src/iosMain/kotlin/app/tivi/common/ui/resources/String.kt
+++ common/ui/resources/strings/src/iosMain/kotlin/app/tivi/common/ui/resources/String.kt
@@ -6,25 +6,31 @@ package app.tivi.common.ui.resources
 import platform.Foundation.NSString
 import platform.Foundation.stringWithFormat
 
-/**
- * https://github.com/icerockdev/moko-resources/blob/c7e3ec78838308aaf9079496a080a12a1adff938/resources/src/appleMain/kotlin/dev/icerock/moko/resources/desc/Utils.kt#L137
- */
-actual fun String.fmt(vararg args: Any?): String {
-    // NSString format works with NSObjects via %@, we should change standard format to %@
-    val fixed = replace(Regex("%((?:\\.|\\d|\\$)*)[abcdefs]"), "%$1@")
+private val PATTERNS_REGEX = "%[\\d|.]*[sdf]".toRegex()
 
-    return when (args.size) {
-        0 -> NSString.stringWithFormat(fixed)
-        1 -> NSString.stringWithFormat(fixed, args[0])
-        2 -> NSString.stringWithFormat(fixed, args[0], args[1])
-        3 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2])
-        4 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3])
-        5 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3], args[4])
-        6 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3], args[4], args[5])
-        7 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3], args[4], args[5], args[6])
-        8 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
-        9 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8])
-        10 -> NSString.stringWithFormat(fixed, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9])
-        else -> error("String.fmt() can only accept up to 10 arguments")
+/**
+ * Yes, this is gross, but it's the only way I could get it to work.
+ * Related: https://youtrack.jetbrains.com/issue/KT-25506
+ */
+@Suppress("USELESS_CAST")
+actual fun String.fmt(vararg args: Any?): String {
+    val formats = PATTERNS_REGEX.findAll(this).map { it.groupValues.first() }.toList()
+
+    var result = this
+
+    formats.forEachIndexed { i, format ->
+        val arg = args[i]
+
+        val formatted = when (arg) {
+            is Double -> NSString.stringWithFormat(format, arg as Double)
+            is Float -> NSString.stringWithFormat(format, arg as Float)
+            is Int -> NSString.stringWithFormat(format, arg as Int)
+            is Long -> NSString.stringWithFormat(format, arg as Long)
+            else -> NSString.stringWithFormat("%@", arg)
+        }
+        result = result.replaceFirst(format, formatted)
     }
+
+    // We put the string through stringWithFormat again, to remove any escaped characters
+    return NSString.stringWithFormat(result, Any())
 }
