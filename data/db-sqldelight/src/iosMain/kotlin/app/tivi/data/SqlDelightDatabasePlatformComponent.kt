37e6bbe2a4cd8bb36711942ff98c04bea8bd5a94
Chris Banes <chris@banes.me>
2023-10-07 10:03:55 +0100

    Try out SQLDelight 2.1.0-SNAPSHOT
    It contains a fix for https://github.com/cashapp/sqldelight/issues/4376,
    so testing that out without my workaround.

--- data/db-sqldelight/src/iosMain/kotlin/app/tivi/data/SqlDelightDatabasePlatformComponent.kt
+++ data/db-sqldelight/src/iosMain/kotlin/app/tivi/data/SqlDelightDatabasePlatformComponent.kt
@@ -3,10 +3,7 @@
 
 package app.tivi.data
 
-import app.cash.sqldelight.Query
-import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import app.cash.sqldelight.driver.native.inMemoryDriver
 import app.tivi.inject.ApplicationScope
@@ -21,7 +18,7 @@ actual interface SqlDelightDatabasePlatformComponent {
         return when {
             configuration.inMemory -> inMemoryDriver(Database.Schema)
             else -> {
-                WorkaroundNativeSqliteDriver(
+                NativeSqliteDriver(
                     schema = Database.Schema,
                     name = "tivi.db",
                     maxReaderConnections = 4,
@@ -30,42 +27,5 @@ actual interface SqlDelightDatabasePlatformComponent {
         }.also { driver ->
             driver.execute(null, "PRAGMA foreign_keys=ON", 0)
         }
-    }
-}
-
-/**
- * [NativeSqliteDriver] wrapper to try and workaround
- * https://github.com/cashapp/sqldelight/issues/4376
- */
-private class WorkaroundNativeSqliteDriver(
-    schema: SqlSchema<QueryResult.Value<Unit>>,
-    name: String,
-    maxReaderConnections: Int = 1,
-) : SqlDriver by NativeSqliteDriver(schema, name, maxReaderConnections) {
-    // We can't use a mutable collection in a HashMap, as the value's hash
-    // will change as the collection changes, which then makes HashMap happy.
-    // In K/N this causes an IllegalStateException (but not on JVM)
-    private val listeners = mutableMapOf<String, Set<Query.Listener>>()
-
-    override fun addListener(vararg queryKeys: String, listener: Query.Listener) {
-        queryKeys.forEach { key ->
-            listeners[key] = (listeners[key] ?: emptySet()) + listener
-        }
-    }
-
-    override fun removeListener(vararg queryKeys: String, listener: Query.Listener) {
-        queryKeys.forEach { key ->
-            listeners[key] = (listeners[key] ?: emptySet()) - listener
-        }
-    }
-
-    @Suppress("UselessCallOnCollection")
-    override fun notifyListeners(vararg queryKeys: String) {
-        queryKeys.flatMap { listeners[it] ?: emptySet() }
-            .asSequence()
-            // This shouldn't be necessary, but adding an extra guard to avoid
-            // https://github.com/cashapp/sqldelight/issues/4376
-            .filterNotNull()
-            .forEach(Query.Listener::queryResultsChanged)
     }
 }
