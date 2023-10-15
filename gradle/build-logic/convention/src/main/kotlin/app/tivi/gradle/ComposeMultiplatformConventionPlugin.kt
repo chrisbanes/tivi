e65c96d6157bd18450355e068402ac1e3aafed66
Chris Banes <chris@banes.me>
2023-10-15 10:02:19 +0100

    Fix spotless

--- gradle/build-logic/convention/src/main/kotlin/app/tivi/gradle/ComposeMultiplatformConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/app/tivi/gradle/ComposeMultiplatformConventionPlugin.kt
@@ -5,8 +5,6 @@ package app.tivi.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.compose.ComposeExtension
 
 class ComposeMultiplatformConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
