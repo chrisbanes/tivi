01ebeb573a4ee06f768e0cd73cab050997415506
Omico <me@omico.icu>
2023-12-28 04:08:01 -0800

    Handle system bar style when the theme changed

--- android-app/app/src/main/kotlin/app/tivi/home/MainActivity.kt
+++ android-app/app/src/main/kotlin/app/tivi/home/MainActivity.kt
@@ -4,10 +4,19 @@
 package app.tivi.home
 
 import android.content.Context
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
@@ -21,6 +30,7 @@ import app.tivi.inject.AndroidActivityComponent
 import app.tivi.inject.AndroidApplicationComponent
 import app.tivi.inject.create
 import app.tivi.screens.DiscoverScreen
+import app.tivi.settings.TiviPreferences
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.rememberCircuitNavigator
 
@@ -36,6 +46,33 @@ class MainActivity : TiviActivity() {
     setContent {
       val backstack = rememberSaveableBackStack { push(DiscoverScreen) }
       val navigator = rememberCircuitNavigator(backstack)
+
+      val isSystemInDarkTheme = isSystemInDarkTheme()
+      val theme by component.applicationComponent.preferences.observeTheme()
+        .collectAsState(TiviPreferences.Theme.SYSTEM)
+      val isDarkTheme by remember {
+        derivedStateOf {
+          when (theme) {
+            TiviPreferences.Theme.SYSTEM -> isSystemInDarkTheme
+            TiviPreferences.Theme.LIGHT -> false
+            TiviPreferences.Theme.DARK -> true
+          }
+        }
+      }
+      LaunchedEffect(isDarkTheme) {
+        enableEdgeToEdge(
+          statusBarStyle = SystemBarStyle.auto(
+            lightScrim = Color.TRANSPARENT,
+            darkScrim = Color.TRANSPARENT,
+            detectDarkMode = { isDarkTheme },
+          ),
+          navigationBarStyle = SystemBarStyle.auto(
+            lightScrim = Color.TRANSPARENT,
+            darkScrim = Color.TRANSPARENT,
+            detectDarkMode = { isDarkTheme },
+          ),
+        )
+      }
 
       component.tiviContent(
         backstack,
