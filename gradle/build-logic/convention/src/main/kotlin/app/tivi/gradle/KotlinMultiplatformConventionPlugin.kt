f65385726505a8c84a0bb3affa43d88beaa7918b
Chris Banes <chrisbanes@users.noreply.github.com>
2024-04-10 10:01:37 +0100

    Upgrade to Kotlin 1.9.23 (#1785)

--- gradle/build-logic/convention/src/main/kotlin/app/tivi/gradle/KotlinMultiplatformConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/app/tivi/gradle/KotlinMultiplatformConventionPlugin.kt
@@ -13,7 +13,9 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon
 
 @OptIn(ExperimentalKotlinGradlePluginApi::class)
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
@@ -71,6 +73,21 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
         compilations.configureEach {
           compilerOptions.configure {
             freeCompilerArgs.add("-Xexpect-actual-classes")
+          }
+        }
+      }
+
+      metadata {
+        compilations.configureEach {
+          if (name == KotlinSourceSet.COMMON_MAIN_SOURCE_SET_NAME) {
+            compileTaskProvider.configure {
+              // We replace the default library names with something more unique (the project path).
+              // This allows us to avoid the annoying issue of `duplicate library name: foo_commonMain`
+              // https://youtrack.jetbrains.com/issue/KT-57914
+              val projectPath = this@with.path.substring(1).replace(":", "_")
+              this as KotlinCompileCommon
+              moduleName.set("${projectPath}_commonMain")
+            }
           }
         }
       }
