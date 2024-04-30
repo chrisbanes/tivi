48ba42458d0428765d2d942866e16ff8e2f0bd72
Chris Banes <chris@banes.me>
2024-04-30 10:50:20 +0100

    Remove old compiler flags

--- gradle/build-logic/convention/src/main/kotlin/app/tivi/gradle/KotlinMultiplatformConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/app/tivi/gradle/KotlinMultiplatformConventionPlugin.kt
@@ -50,17 +50,6 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
         compilations.configureEach {
           compileTaskProvider.configure {
             compilerOptions {
-              // Try out preview custom allocator in K/N 1.9
-              // https://kotlinlang.org/docs/whatsnew19.html#preview-of-custom-memory-allocator
-              freeCompilerArgs.add("-Xallocator=custom")
-
-              // https://kotlinlang.org/docs/whatsnew19.html#compiler-option-for-c-interop-implicit-integer-conversions
-              freeCompilerArgs.add("-XXLanguage:+ImplicitSignedToUnsignedIntegerConversion")
-
-              // Enable debug symbols:
-              // https://kotlinlang.org/docs/native-ios-symbolication.html
-              freeCompilerArgs.add("-Xadd-light-debug=enable")
-
               // Various opt-ins
               freeCompilerArgs.addAll(
                 "-opt-in=kotlinx.cinterop.ExperimentalForeignApi",
@@ -73,8 +62,10 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
 
       targets.configureEach {
         compilations.configureEach {
-          compilerOptions.configure {
-            freeCompilerArgs.add("-Xexpect-actual-classes")
+          compileTaskProvider.configure {
+            compilerOptions {
+              freeCompilerArgs.add("-Xexpect-actual-classes")
+            }
           }
         }
       }
