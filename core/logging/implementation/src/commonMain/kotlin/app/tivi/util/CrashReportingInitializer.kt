81e33a97b485ab6ce00ce4ff5e6ea22a38792f3c
Chris Banes <chris@banes.me>
2024-07-13 11:12:25 +0300

    Read Preference into a StateFlow
    Should help with preferences always starting from the
    default value

--- core/logging/implementation/src/commonMain/kotlin/app/tivi/util/CrashReportingInitializer.kt
+++ core/logging/implementation/src/commonMain/kotlin/app/tivi/util/CrashReportingInitializer.kt
@@ -6,22 +6,19 @@ package app.tivi.util
 import app.tivi.appinitializers.AppInitializer
 import app.tivi.inject.ApplicationCoroutineScope
 import app.tivi.settings.TiviPreferences
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import me.tatarka.inject.annotations.Inject
 
 @Inject
 class CrashReportingInitializer(
   private val preferences: Lazy<TiviPreferences>,
+  private val action: Lazy<SetCrashReportingEnabledAction>,
   private val scope: ApplicationCoroutineScope,
-  private val action: SetCrashReportingEnabledAction,
-  private val dispatchers: AppCoroutineDispatchers,
 ) : AppInitializer {
   override fun initialize() {
     scope.launch {
       preferences.value.reportAppCrashes.flow
-        .flowOn(dispatchers.io)
-        .collect(action::invoke)
+        .collect { action.value(it) }
     }
   }
 }
